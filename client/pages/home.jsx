7684568e3bf3750dff3b78cc4ac7352917249eb4
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-04 19:20:09 +0000

    Home pg HTML updated & inline CSS styles added

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -4,14 +4,68 @@ import Jar from '../components/jar';
 import Basket from '../components/basket';
 import Backpack from '../components/backpack';
 
+const homeStyle = {
+  display: 'flex',
+  flexDirection: 'column'
+};
+
+const rowStyle = {
+  display: 'flex',
+  justifyContent: 'flex-end'
+};
+
+const boxStyle = {
+  justifyContent: 'flex-end'
+};
+
+const basketStyle = {
+  justifyContent: 'flex-end'
+};
+
 export default function Home(props) {
   return (
-      <div>
-        <Box />
-        <Jar />
-        <Basket />
-        <Backpack />
-
+      <div style={homeStyle} className="container">
+        <div style={rowStyle} className="row">
+          <div style={boxStyle} className="column-2 box">
+            <td>
+              <Box />
+              <td>
+                <button>Build a Box</button>
+              </td>
+            </td>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column-2 jar">
+            <td>
+              <Jar />
+              <td>
+                <button>Make a Jar</button>
+              </td>
+            </td>
+          </div>
+        </div>
+      <div style={rowStyle} className="row">
+          <div style={basketStyle} className="column-2">
+            <td>
+              <Basket />
+              <td>
+                <button>Make a Basket</button>
+              </td>
+            </td>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column-2">
+            <td>
+              <Backpack />
+              <td>
+                <button>Make a Backpack</button>
+              </td>
+            </td>
+          </div>
+        </div>
       </div>
   );
+
 }
