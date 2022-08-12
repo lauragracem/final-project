d249dfd3c6078ec83f5d8be9c7ace590c9802e25
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-12 20:32:53 +0000

    Changed jar png images to svg images

--- client/components/containerJars.jsx
+++ client/components/containerJars.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function ContainerJars(props) {
   return (
     <div style={{ height: '100vh' }} className="row d-flex justify-content-center align-items-center">
-      <img className="col-xs-3 p-4 mt-3" key="first" src="images/green-jar-1.png"></img>
-      <img className="col-xs-3 p-4 mt-3" key="second" src="images/green-jar-2.png"></img>
-      <img className="col-xs-3 p-4 mt-3" key="third" src="images/green-jar-3.png"></img>
+      <img className="col-xs-3 p-4 mt-3" key="first" src="images/green-jar-1.svg"></img>
+      <img className="col-xs-3 p-4 mt-3" key="second" src="images/green-jar-2.svg"></img>
+      <img className="col-xs-3 p-4 mt-3" key="third" src="images/green-jar-3.svg"></img>
     </div>
   );
 }
