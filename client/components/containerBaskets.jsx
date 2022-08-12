e0823f427e163c64f7c9d4ea1076e0f534170cb3
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-12 20:48:41 +0000

    Changed basket png images to svg images

--- client/components/containerBaskets.jsx
+++ client/components/containerBaskets.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ContainerBaskets(props) {
   return (
     <div style={{ height: '100vh' }} className="row d-flex justify-content-center align-items-center">
-      <img className="col-xs-3 mb-0 mt-5 mr-md-5" src="images/green-basket-1.png"></img>,
-      <img className="col-xs-3 mb-xs-5 ml-md-5" src="images/green-basket-2.png"></img>
+      <img className="col-xs-3 mb-0 mt-5 mr-md-5" src="images/green-basket-1.svg"></img>,
+      <img className="col-xs-3 mb-xs-5 ml-md-5" src="images/green-basket-2.svg"></img>
     </div>
   );
 }
