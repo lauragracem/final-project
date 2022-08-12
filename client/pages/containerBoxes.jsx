05de9f273432578f128228f2d3f033a722d3f310
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-12 20:27:05 +0000

    Edited bootstrap support classes for containerBoxes

--- client/pages/containerBoxes.jsx
+++ client/pages/containerBoxes.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ContainerBoxes from '../components/containerBoxes';
 
-const style = {
-  display: 'flex',
-  justifyContent: 'center',
-  align: 'center'
-};
-
 export default function containers(props) {
   return (
-    <div style={style} className="container">
+    <div className="container">
       <ContainerBoxes />
     </div>
   );
