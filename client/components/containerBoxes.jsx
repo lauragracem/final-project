3ffb09e4751795f1bfb8cb8c9b0940922c6b4741
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-12 20:52:58 +0000

    Edited bootstrap support classes for containerBaskets, containerBoxes & containerJars

--- client/components/containerBoxes.jsx
+++ client/components/containerBoxes.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ContainerBoxes(props) {
   return (
-    <div style={{ height: '100vh' }} className="row d-flex flex-xs-column flex-sm-column flex-md-row justify-content-center align-items-center">
-      <img className="col-xs-6 d-flex mt-sm-0 mt-md-0 mr-md-5 mx-5" src="images/green-box.svg" />
-      <img className="col-xs-6 d-flex mb-sm-0 mt-md-0 ml-md-5 mx-5" src="images/green-lunch.svg" />
+    <div style={{ height: '100vh' }} className="row d-flex flex-md-row justify-content-center align-items-center">
+      <img className="col-xs-6 mt-sm-0 mt-md-0 mr-md-5 mx-5" src="images/green-box.svg" />
+      <img className="col-xs-6 mb-sm-0 mt-md-0 ml-md-5 mx-5" src="images/green-lunch.svg" />
     </div>
   );
 
