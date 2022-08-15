a30dd04eceb48c40a9588b20b75f8306881d8452
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-15 18:57:17 +0000

    Created colorChoices component & edited bootstrap support classes for containerBoxes page

---
+++ client/components/colorChoices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Colors(props) {
+  return (
+    <div className="row d-flex justify-content-space-between pt-3">
+      <img className="d-flex justify-content-space-between pt-5 pb-3 pr-5" src="images/BlueColorSquare.svg"></img>
+      <img className="d-flex justify-content-space-between pt-5 pb-3 pr-5" src="images/YellowColorSquare.svg"></img>
+      <img className="d-flex justify-content-space-between pt-5 pb-3" src="images/PeachColorSquare.svg"></img>
+    </div>
+  );
+}
--- client/pages/containerBoxes.jsx
+++ client/pages/containerBoxes.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ContainerBoxes from '../components/containerBoxes';
+import Colors from '../components/colorChoices';
 
 export default function containers(props) {
   return (
-    <div className="container">
+    <div style={{ height: '100vh' }} className="container row d-flex flex-md-row justify-content-center align-items-center ml-0 mr-0 pl-0 pr-0">
+      <Colors />
       <ContainerBoxes />
     </div>
   );
