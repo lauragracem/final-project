65633804ccd08ca7a41ece8366b9db14f8dcc9d6
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-16 22:26:27 +0000

    Used useState to set color options

--- client/components/colorChoices.jsx
+++ client/components/colorChoices.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Colors(props) {
+export default function Colors({ setColor }) {
+
   return (
     <div className="row d-flex flex-row justify-content-center my-5">
-      <img className="mx-4" src="images/BlueColorSquare.svg"></img>
-      <img className="mx-4" src="images/YellowColorSquare.svg"></img>
-      <img className="mx-4" src="images/PeachColorSquare.svg"></img>
+      <img className="mx-4" src="images/BlueColorSquare.svg" onClick={() => setColor('blue')} />
+      <img className="mx-4" src="images/YellowColorSquare.svg" onClick={() => setColor('yellow')} />
+      <img className="mx-4" src="images/PeachColorSquare.svg" onClick={() => setColor('peach')} />
     </div>
   );
 }
--- client/components/peach-boxes.jsx
+++ client/components/peach-boxes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PeachBox(props) {
   return (
-    <div className="row d-flex flex-row justify-content-center align-items-center mt-5 pt-5 visible position-absolute">
+    <div className="row d-flex flex-row justify-content-center align-items-center mt-5 pt-5">
       <img className="d-flex col-xs-6 mt-5 mr-md-5 mx-5" src="images/peachBox.svg"></img>
       <img className="d-flex col-xs-6 mt-5 ml-md-5 mx-5" src="images/peachLunch.svg"></img>
     </div>
