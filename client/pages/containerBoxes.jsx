a0f2eb68c7f165533cb586e0b5ed8b614d554fb5
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-16 20:01:03 +0000

    Added blue-boxes components to containerBoxes page & edited bootstrap support classes

--- client/pages/containerBoxes.jsx
+++ client/pages/containerBoxes.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ContainerBoxes from '../components/containerBoxes';
+import BlueBox from '../components/blue-boxes';
 import Colors from '../components/colorChoices';
 
 export default function containers(props) {
+
   return (
     <div style={{ height: '100vh', width: '100vw' }} className="container d-flex flex-column justify-content-center align-items-center">
       <Colors />
       <ContainerBoxes />
+      <BlueBox />
     </div>
   );
 }
