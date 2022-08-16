52c5f5f54e9bcb33fd1ff948bfefd09755645a49
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-16 01:18:33 +0000

    Edited bootstrap support classes for containerBackpacks page 1

--- client/components/containerBackpacks.jsx
+++ client/components/containerBackpacks.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ContainerBackpacks(props) {
   return (
-    <div className="row d-flex justify-content-center align-items-center my-2">
+    <div className="row d-flex justify-content-center align-items-center">
       <img className="col-xs-3 mr-md-5 mx-5" src="images/green-backpack-1.svg"></img>
-      <img className="col-xs-3 ml-md-5 mx-5" src="images/green-backpack-2.svg"></img>
+      <img className="col-xs-3 ml-md-5 my-5 mx-5" src="images/green-backpack-2.svg"></img>
     </div>
   );
 }
