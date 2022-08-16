c88fb1f223d30a56ad08eec4bad8ea151d3ea1ac
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-16 00:29:14 +0000

    Edited bootstrap support classes for containerJars page - note: fix mobile spacing on y-axis for iPhone SE

--- client/pages/containerJars.jsx
+++ client/pages/containerJars.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ContainerJars from '../components/containerJars';
+import Colors from '../components/colorChoices';
 
 export default function Jars(props) {
   return (
-    <div className="container">
+    <div style={{ height: '100vh' }} className="container d-flex flex-column justify-content-center align-items-center my-5">
+      <Colors />
       <ContainerJars />
     </div>
   );
