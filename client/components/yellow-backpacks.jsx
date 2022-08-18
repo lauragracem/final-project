68081217184441c4271f13d2f50664f2559ebcff
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 20:07:06 +0000

    Edited bootstrap support classes for backpack components

--- client/components/yellow-backpacks.jsx
+++ client/components/yellow-backpacks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function YellowBackpacks(props) {
   return (
-    <div className="row d-flex flex-row justify-content-center align-items-center mt-5">
+    <div className="row d-flex justify-content-center align-items-center">
       <img className="d-flex col-xs-6 mt-sm-0 mt-md-0 mr-md-5 mx-5 pb-5" src="images/yellowBackpack1.svg"></img>
       <img className="d-flex col-xs-6 mb-sm-0 mt-md-0 ml-md-5 mx-5 pb-5" src="images/yellowBackpack2.svg"></img>
     </div>
