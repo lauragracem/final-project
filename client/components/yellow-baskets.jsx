32783d804f85d2283cec4815edcb30053db0f004
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 18:56:11 +0000

    Created component for yellow-baskets

---
+++ client/components/yellow-baskets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function YellowBaskets(props) {
+  return (
+    <div>
+      <img src="images/yellowBaskets1.svg"></img>
+      <img src="images/yellowBaskets2.svg"></img>
+    </div>
+  );
+}
