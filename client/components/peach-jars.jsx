f4d1607055c3848bcc6079edce372294937c724f
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 18:50:52 +0000

    Set useState for all color components & updated bootstrap support classes

---
+++ client/components/peach-jars.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function PeachJars(props) {
+  return (
+    <div style={{ width: '100vw' }} className="row d-flex flex-row justify-content-center align-items-center mb-5">
+      <img className="col-xs-3 p-4 my-3 mx-5" src="images/peachJar1.svg"></img>
+      <img className="col-xs-3 p-4 my-3 mx-5" src="images/peachJar2.svg"></img>
+      <img className="col-xs-3 p-4 my-3 mx-5" src="images/peachJar3.svg"></img>
+    </div>
+  );
+}
--- client/pages/containerJars.jsx
+++ client/pages/containerJars.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContainerJars from '../components/containerJars';
+import BlueJars from '../components/blue-jars';
+import YellowJars from '../components/yellow-jars';
+import PeachJars from '../components/peach-jars';
 import Colors from '../components/colorChoices';
 
 export default function Jars(props) {
+
+  const [color, setColor] = useState('green');
+
   return (
     <div style={{ height: '100vh' }} className="container d-flex flex-column justify-content-center align-items-center my-5">
-      <Colors />
-      <ContainerJars />
+      <Colors setColor={setColor} />
+      {color === 'green' && <ContainerJars />}
+      {color === 'blue' && <BlueJars />}
+      {color === 'yellow' && <YellowJars />}
+      {color === 'peach' && <PeachJars />}
     </div>
   );
 }
