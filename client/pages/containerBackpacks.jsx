a684d69e176e11bb4d3e806f5347d2b610cbc937
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 20:04:04 +0000

    Created components for peach-backpacks and updated bootstrap support classes

--- client/pages/containerBackpacks.jsx
+++ client/pages/containerBackpacks.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContainerBackpacks from '../components/containerBackpacks';
+import BlueBackpacks from '../components/blue-backpacks';
+import YellowBackpacks from '../components/yellow-backpacks';
+import PeachBackpacks from '../components/peach-backpacks';
 import Colors from '../components/colorChoices';
 
 export default function ContainerSubTypesThree(props) {
+
+  const [color, setColor] = useState('green');
+
   return (
     <div style={{ height: '100vh' }} className="container d-flex flex-column justify-content-center align-items-center">
-      <Colors />
-      <ContainerBackpacks />
+      <Colors setColor={setColor} />
+      {color === 'green' && <ContainerBackpacks />}
+      {color === 'blue' && <BlueBackpacks />}
+      {color === 'yellow' && <YellowBackpacks />}
+      {color === 'peach' && <PeachBackpacks />}
     </div>
   );
 }
