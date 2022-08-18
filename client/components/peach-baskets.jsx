490d7522eb4607898539ff2d3b67f67fc7971e27
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 19:05:24 +0000

    Created component for peach-baskets

---
+++ client/components/peach-baskets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PeachBaskets(props) {
+  return (
+    <div>
+      <img src="images/peachBasket1.svg"></img>
+      <img src="images/peachBasket2.svg"></img>
+    </div>
+  );
+}
