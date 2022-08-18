2a3813ec06ddd26095f023f7c4b1dbeb503b3691
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-18 18:54:10 +0000

    Created component for blue-baskets

---
+++ client/components/blue-baskets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BlueBaskets(props) {
+  return (
+    <div>
+      <img src="images/blueBasket1.svg"></img>
+      <img src="images/blueBasket2.svg"></img>
+    </div>
+  );
+}
