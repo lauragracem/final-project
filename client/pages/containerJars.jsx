2f2afb4bac48348e0a0eb68ebc047b4136e87231
Laura Grace Manimtim <lauragracem@gmail.com>
2022-11-02 19:26:56 +0000

    Added useNavigate to containerJars

--- client/pages/containerJars.jsx
+++ client/pages/containerJars.jsx
@@ -5,10 +5,12 @@ import YellowJars from '../components/yellow-jars';
 import PeachJars from '../components/peach-jars';
 import Colors from '../components/colorChoices';
 import BackArrow from '../components/backArrow';
+import { useNavigate } from 'react-router-dom';
 
 export default function Jars(props) {
 
   const [color, setColor] = useState('green');
+  const navigate = useNavigate();
 
   return (
     <div style={{ height: '100vh' }} className="container d-flex flex-column justify-content-center align-items-center my-5">
@@ -17,7 +19,9 @@ export default function Jars(props) {
       {color === 'blue' && <BlueJars />}
       {color === 'yellow' && <YellowJars />}
       {color === 'peach' && <PeachJars />}
-      <BackArrow/>
+      <div onClick={() => navigate('/#')}>
+        <BackArrow />
+      </div>
     </div>
   );
 }
